dd7c5eed2350c928bea75559ac7a72e703c15e63
BooojChai <just_cbj@sina.com>
2022-06-14 22:06:26 +0800

    Import ele

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <HomePage/>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <!-- <HomePage/> -->
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+//import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
-  components: {
-    HomePage
-  }
+  // components: {
+  //   HomePage
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import { Button } from 'element-ui';
+import App from './App.vue';
 import "./assets/iconfont/iconfont.css"
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
 
 new Vue({
   render: h => h(App),
